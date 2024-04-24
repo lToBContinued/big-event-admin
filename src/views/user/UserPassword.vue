43d297aa8a6424a5a83b95aa7a1e1e66a8d12816
lToBContinued <3212834434@qq.com>
2024-04-24 13:09:35 +0800

    完善修改密码功能

--- src/views/user/UserPassword.vue
+++ src/views/user/UserPassword.vue
@@ -2,9 +2,36 @@
 import PageContainer from '@/components/pageContainer.vue'
 import { ref } from 'vue'
 import { userUpdatePasswordService } from '@/api/user.js'
-import { useUserStore } from '@/stores/index.js'
+import { useUserStore } from '@/stores'
 import { useRouter } from 'vue-router'
 
+const router = useRouter()
+const formRef = ref(null)
+const userStore = useUserStore()
+
+const formModel = ref({
+  old_pwd: '',
+  new_pwd: '',
+  re_pwd: ''
+})
+
+// 表单校验
+const checkDifferent = (rule, value, callback) => {
+  // 新密码和原密码不能一样
+  if (value === formModel.value.old_pwd) {
+    callback(new Error('新密码不能和原密码一样'))
+  } else {
+    callback()
+  }
+}
+const checkSameAsNewPwd = (rule, value, callback) => {
+  // 校验确认密码
+  if (value !== formModel.value.new_pwd) {
+    callback(new Error('确认密码必须和新密码一样'))
+  } else {
+    callback()
+  }
+}
 const rules = {
   old_pwd: [
     { required: true, message: '原密码不能为空', trigger: 'blur' },
@@ -20,6 +47,10 @@ const rules = {
       pattern: /^\S{6,15}$/,
       message: '密码必须是6~15位非空字符',
       trigger: 'blur'
+    },
+    {
+      validator: checkDifferent,
+      trigger: 'blur'
     }
   ],
   re_pwd: [
@@ -30,37 +61,27 @@ const rules = {
       trigger: 'blur'
     },
     {
-      validator: (rule, value, callback) => {
-        // 判断 value 和当前 form 中收集的 password 是否一致
-        if (value !== formModel.value.new_pwd) {
-          callback(new Error('两次输入的密码不一致'))
-        } else {
-          callback() // 就算校验成功，也需要callback
-        }
-      },
+      validator: checkSameAsNewPwd,
       trigger: 'blur'
     }
   ]
 }
-
-const router = useRouter()
-const formRef = ref(null)
-const userStore = useUserStore()
-
-const formModel = ref({
-  old_pwd: '',
-  new_pwd: '',
-  re_pwd: ''
-})
 
 // 提交修改密码
 const onSubmit = async () => {
   await formRef.value.validate()
   await userUpdatePasswordService(formModel.value)
   ElMessage.success('修改密码成功，请重新登录')
-  userStore.token = ''
-  userStore.user = {}
+  // 清空用户数据
+  userStore.setToken('')
+  userStore.setUser({})
+  // 拦截到登录页面
   setTimeout(async () => await router.push('/login'), 1500)
+}
+
+// 清空输入框
+const onReset = () => {
+  formRef.value.resetFields()
 }
 </script>
 
@@ -93,6 +114,7 @@ const onSubmit = async () => {
       </el-form-item>
       <el-form-item label-width="100px">
         <el-button type="primary" @click="onSubmit">修改密码</el-button>
+        <el-button @click="onReset">重置</el-button>
       </el-form-item>
     </el-form>
   </page-container>
